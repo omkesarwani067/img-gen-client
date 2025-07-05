0a278f050771f6b735d96bef01eebd91596a32db
omkesarwani067 <omkesarwani067@gmail.com>
2025-07-05 16:14:05 +0530

    ch

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,22 @@ import { Routes, Route } from 'react-router-dom'
 import BuyCredit from "./pages/Buycredit"
 import Home from "./pages/Home"
 import Result from "./pages/Result"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Login from "./components/Login"
+import Navbar from './components/Navbar'  // Updated path
+import Login from "./components/Login"    // Updated path
+import Footer from "./components/Footer"  // Updated path
 
 import { useContext } from 'react'
 import { AppContext } from './context/AppContext'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const App = () => {
-
   const {showLogin} = useContext(AppContext)
-
 
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-red-200 to-purple-300'>
       <ToastContainer position='bottom-right'/>
       <Navbar />
- 
       
       {showLogin && <Login/>}
       <Routes>
