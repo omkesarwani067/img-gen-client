c9f0658606c2a66d20cce61030fae0340d4e1e05
omkesarwani067 <omkesarwani067@gmail.com>
2025-07-26 18:09:55 +0530

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="flex items-center justify-between gap-4 py-3 mt-20">
       <img src={assets.logo} alt="" width={150} />
       <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
-        Copyright @Nandhan.dev | All rights reserved .
+        Copyright @KHAWABOM.dev | All rights reserved .
       </p>
       <div className="flex gap-2.5">
         <img src={assets.facebook_icon} alt="" width={35} />
